4635ac45146d52bf04e30062423c049942de725e
Katie <s.jkatie@yahoo.com>
2015-09-02 00:16:09 -0400

    homework
    wk7

---
+++ js/js.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+console.log(userChoice);
+var computerChoice = Math.random()
+console.log (computerChoice)
+if(0-.33)
+console.log("rock")
+else if(.34-.66)
+console.log("paper")
+else(.67-1)
+console.log("scissors")
+
+var compare = function(choice1, choice2) {
+if (choice1 === choice2) {
+return "The result is a tie!";
+}
+else if (choice1 ==="rock") {
+if (choice2 === "scissors") {
+return "rock wins";
+}
+else {
+return "paper wins";
+}
+}
+else if (choice1 ==="paper") {
+if (choice2 ==="rock") {
+return "paper wins";
+}
+else {
+return "scissors wins";
+}
+}
+};
